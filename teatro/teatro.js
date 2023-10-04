1c331a97fb1cab2f1cafa5dfd4ecf3dfe26575cc
rafaelminski <rafael.minski57@gmail.com>
2023-10-04 01:27:45 -0300

    atualização teatro

--- teatro/teatro.js
+++ teatro/teatro.js
@@ -4,14 +4,14 @@ const dvPalco = document.querySelector("#divPalco");
 
 const POLTRONAS = 240; //quantidade de poltronas
 
-const reservadas = []; // vetor com as poltronas reservadas 
+const reservadas = [ ]; // vetor com as poltronas reservadas 
 
 window.addEventListener("load", () => {
     const ocupadas = localStorage.getItem("teatroOcupadas")
-    ? localStorage.getItem("teatroOcupadas").split(";")
-    : [];
+    ? localStorage.getItem("teatroOcupadas").split(" ; ")
+    : [] ;
 
-    for(let i = 1;i<=POLTRONAS;i++){
+    for(let i = 1; i <= POLTRONAS ; i++){
         const figure = document.createElement("figure");
         const imgStatus = document.createElement("img");
 
@@ -23,7 +23,7 @@ window.addEventListener("load", () => {
         const figureCap = document.createElement("figcaption");
 
         //quantidade de zeros antes do número da poltrona
-        const zeros = i < 10 ? " 00 " : i < 100 ? " 0 " : " ";
+        const zeros = i < 10 ? "00" : i < 100 ? "0" : "";
 
         //cria o número da poltrona
         const num = document.createTextNode(`[${zeros}${i}]`)
@@ -32,6 +32,89 @@ window.addEventListener("load", () => {
         figure.appendChild(imgStatus);
         figure.appendChild(figureCap);
 
-        dvPalco.appendChild(figure);
+        if(i % 24 == 12 ) figure.style.marginRight = "60px";
+
+
+        divPalco.appendChild(figure);
+
+        if (i % 24 == 0 && divPalco.appendChild(document.createElement("br")));
+        
     }
 })
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const poltrona = Number(frm.inPoltrona.value);
+    const ocupadas = localStorage.getItem("teatroOcupadas")
+    ? localStorage.getItem("teatroOcupadas").split(";")
+    : [];
+
+    if (poltrona > POLTRONAS) {
+        alert("Informe um número de poltrona válido");
+        frm.inPoltrona.value = "";
+        frm.inPoltrona.focus();
+        return;
+    }
+    if(ocupadas.includes(poltrona.toString())){
+        alert(`Poltrona ${poltrona} já está reservada`);
+        frm.inPoltrona.value = "";
+        frm.inPoltrona.focus();
+        return;
+    }
+   
+    //captura a imagem da poltrona , filha de divPalco. é -1 pois começa em 0
+    const imgPoltrona = dvPalco.querySelectorAll("img")[poltrona -1];
+     imgPoltrona.src = "img/reservada.jpg" //modifica o atributo da imagem
+    reservadas.push(poltrona);
+
+    frm.inPoltrona.value = "";
+    frm.inPoltrona.focus();
+
+   
+
+})
+
+frm.btnConfirmar.addEventListener("click", () => {
+    if(reservadas.length == 0){
+        alert("Não há poltronas reservadas")
+        frm.inPoltrona.focus();
+        return;
+    }
+    const ocupadas = localStorage.getItem("teatroOcupadas")
+    ? localStorage.getItem("teatroOcupadas").split(";")
+    : [];
+
+    for (let i = reservadas.length -1; i >= 0; i--){
+        ocupadas.push(reservadas[i]);
+        const imgPoltrona = dvPalco.querySelectorAll("img")[reservadas[i] -1];
+        imgPoltrona.src = "img/ocupada.jpg"
+
+        reservadas.pop();
+    }
+
+    localStorage.setItem("teatroOcupadas", ocupadas.join(";"));
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
